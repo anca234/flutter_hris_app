86cf22e3fe29abf6515f808b02dd61ada13135f1
anca <muhammad.rafliansyah002@gmail.com>
2025-01-07 17:13:41 +0700

    profile screen jadi menggunakan SingleChildScrollView

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -20,63 +20,66 @@ class ProfileScreen extends StatelessWidget {
         ],
       ),
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-      body: SingleChildScrollView(child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 20),
-            const Center(
-              child: CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.grey,
-              ),
-            ),
-            const SizedBox(height: 20),
-            const TextField(
-              decoration: InputDecoration(labelText: 'Employee Name:'),
-            ),
-            const TextField(
-              decoration: InputDecoration(labelText: 'Job Title:'),
-            ),
-            const TextField(
-              decoration: InputDecoration(labelText: 'Department:'),
-            ),
-            const TextField(
-              decoration: InputDecoration(labelText: 'Employee ID:'),
-            ),
-            const TextField(
-              decoration: InputDecoration(labelText: 'Email:'),
-            ),
-            const TextField(
-              decoration: InputDecoration(labelText: 'Phone:'),
-            ),
-            const TextField(
-              decoration: InputDecoration(labelText: 'Date of Birth:'),
-            ),
-            const TextField(
-              decoration: InputDecoration(labelText: 'Address:'),
-            ),
-            const SizedBox(height: 20),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('Edit Profile'),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Center(
-              child: TextButton(
-                onPressed: () {},
-                child: const Text(
-                  'more detail',
-                  style: TextStyle(color: Colors.blue),
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              const Center(
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.grey,
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Employee Name:'),
+              ),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Job Title:'),
+              ),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Department:'),
+              ),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Employee ID:'),
+              ),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Email:'),
+              ),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Phone:'),
+              ),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Date of Birth:'),
+              ),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Address:'),
+              ),
+              const SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Edit Profile'),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Center(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'more detail',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),)
+      ),
     );
   }
 }
