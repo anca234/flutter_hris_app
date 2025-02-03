9afcc6aff7f2e96c9c4aa721b73dd3362150174b
anca <muhammad.rafliansyah002@gmail.com>
2025-02-03 17:31:36 +0700

    up version to 3 & change app name to HC_Apitec

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "id.co.ptap.hcmobile"
         minSdk = 21 // Replace with your app's minimum SDK level
         targetSdk = 34
-        versionCode = 2 //update versioncode harus selalu lebih besar dari versioncode sebelumnya
-        versionName = "1.0.2"
+        versionCode = 3 //update versioncode harus selalu lebih besar dari versioncode sebelumnya
+        versionName = "1.0.3"
     }
 
     buildTypes {
